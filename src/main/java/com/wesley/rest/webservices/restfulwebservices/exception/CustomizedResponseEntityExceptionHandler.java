832797bc10ec76dd389639589e3e5dcb4f7e5dc0
WesBourkeMusic <wesbourkeavl@gmail.com>
2023-02-23 23:10:24 -0500

    error handling, I need more work in this area.

--- src/main/java/com/wesley/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/wesley/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,21 +1,40 @@
 package com.wesley.rest.webservices.restfulwebservices.exception;
 
+import java.time.LocalDateTime;
+
+import com.wesley.rest.webservices.restfulwebservices.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
 
-public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+    //users not found exception
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(),
-                ex.getMessage(),
-                request.getDescription(false));
-        new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+
     }
+
+    //user not found
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+
+    }
+
 
 }
--- src/main/java/com/wesley/rest/webservices/restfulwebservices/exception/ErrorDetails.java
+++ src/main/java/com/wesley/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -5,26 +5,27 @@ package com.wesley.rest.webservices.restfulwebservices.exception;
 //This is starting to feel like a model
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 //I want a timme stamp when the error happened
 //I want an error message
 //I want to store the details.
 
 public class ErrorDetails{
-    private LocalDate timestamp;
+    private LocalDateTime timestamp;
     private String message;
     private String details;
 
-    public ErrorDetails(LocalDate timestamp, String message, String details) {
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
         this.timestamp = timestamp;
         this.message = message;
         this.details = details;
     }
 
-    public LocalDate getTimestamp() {
+    public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
-    public void setTimestamp(LocalDate timestamp) {
+    public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
     }
 
